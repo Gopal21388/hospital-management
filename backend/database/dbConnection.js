86cc422c6f3ac30fe2a2574ed100080eb01e68fb
Gopal21388 <gopalrajak86276@gmail.com>
2024-12-06 17:20:13 +0530

    update

---
+++ backend/database/dbConnection.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv"
+export const dbConnection = () => {
+  mongoose
+    .connect(process.env.MONGO_URI, {
+      dbName: "Mern",
+    })
+    .then(() => {
+      console.log("Connected to database!");
+    })
+    .catch((err) => {
+      console.log(`Some error occured while connecting to database:, ${err}`);
+    });
+};
